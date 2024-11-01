e5ae399155484818c5e293c39f227366352b958c
ouchin55edcx <ouchinmustapha82@gmail.com>
2024-11-01 09:55:20 +0100

    Owner entity

---
+++ src/main/java/com/ouchin/ITLens/entity/Owner.java
@@ -0,0 +1,21 @@
+package com.ouchin.ITLens.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Owner {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
